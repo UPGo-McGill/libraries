f7fc468c8caf75e29f6df49893553eff310a70c1
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-09 16:44:48 -0400

    buffer and union

---
+++ R/buffer_union.R
@@ -0,0 +1,19 @@
+### BUFFER LIBRARIES ################################################################
+
+source("R/helper_functions.R")
+
+## Buffer 1000 m = 1 km
+
+lib_buffer <- st_buffer(Libraries, 1000)%>%
+  st_geometry ()
+plot(lib_buffer)
+
+## Sort by CMA?
+# Might need to use st_combine, to combine all buffers within a CMA 
+
+## Union overlaping buffers
+
+lib_union <- st_union (lib_buffer)
+plot(lib_union)
+  
+
