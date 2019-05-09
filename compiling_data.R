cce9c4453d6317fcb41bc8c784fef0ee6492e345
amy <amy.bolt@mail.mcgill.ca>
2019-05-09 11:33:33 -0400

    write csv

--- compiling_data.R
+++ compiling_data.R
@@ -1,6 +1,6 @@
 ## Clean and Compile Library Data
 ## May 8, 2019
-install.packages("dplyr")
+
 library(tidyverse)
 library(dplyr)
 
@@ -107,6 +107,7 @@ names(Other) <- c("Library_System", "Library_Branch", "Latitude", "Longitude")
 Canadian_libraries <- rbind(Saskatoon, New_Brunswick, London, Waterloo, Winnipeg, Windsor,
                             Kitchener, Idea_Exchange, Toronto, St_Cats, Halifax, Markham, Other) 
 
+write.csv(Canadian_libraries, file = "Canadian_libraries.csv")
 
 library(ggplot2)
 library(ggmap)
@@ -115,7 +116,7 @@ library(mapdata)
 
 
 Canadian_libraries
-install.packages("sf")
+# install.packages("sf")
 library(sf)
 Can_Lib = st_as_sf(Canadian_libraries, coords = c("Longitude", "Latitude"),
                    crs=4326) 
