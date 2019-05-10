637df4407fbcb2c3e3d1eed7250155c9d6c18390
amy <amy.bolt@mail.mcgill.ca>
2019-05-10 11:48:27 -0400

    updated

--- R/mapping.R
+++ R/mapping.R
@@ -5,7 +5,7 @@ source("R/data_import.R")
 source("R/buffer_union.R")
 
 
-CTs <- st_join(CTs, CMA_name, by = "CMAs" )
+# CTs <- st_join(CTs, CMA_name, by = "CMAs" )
 
 lib_buffer <- st_buffer(Libraries, 1000)%>%
   st_geometry ()
@@ -19,14 +19,25 @@ lib_union <- st_union (lib_buffer)
 # Toronto 
 tm_shape(filter(CMAs, name == "Toronto (B)")) + 
   tm_borders(col = 'black') +
-tm_shape(filter(CTs, CMA_UID == "35535")) +
-  tm_polygons(c("v_CA16_2354: Percentage with employment income", "Population"), border.alpha = 0)  + 
+tm_shape(filter(CTs_test, CMA_UID == "35535")) +
+  tm_polygons(c("Population", "v_CA16_2354: Percentage with employment income", 
+                "v_CA16_2398: Median after-tax income of households in 2015 ($)", 
+                "v_CA16_4888: Spending 30% or more of income on shelter costs", 
+                "v_CA16_488: Total lone-parent families by sex of parent", 
+                "v_CA16_3411: Immigrants", 
+                "v_CA16_3957: Total visible minority population"), border.alpha = 0)  + 
   tm_facets(sync = TRUE, ncol = 2) +
-tm_shape(lib_union)+
+tm_shape(lib_union) +
   tm_borders(col = 'black') + 
   tm_layout(main.title = "Toronto", 
             main.title.position = "left", 
-            title = c("Percentage of Population on EI", "Population"), 
+            title = c("Population", 
+                      "Percentage with Employment Income",
+                      "Median After-Tax Household Income",
+                      "Percentage with Core Housing Need",
+                      "Percentage of Single-Parent Families",
+                      "Percentage Immigrants",
+                      "Percentage Visible Minority"), 
             legend.position = c("left", "top"),
             frame = FALSE) +
   tm_compass()
