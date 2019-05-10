f75a2aa092eb389da13bb3e92f8f9d72098db239
amy <amy.bolt@mail.mcgill.ca>
2019-05-10 11:16:54 -0400

    formatting

--- R/mapping.R
+++ R/mapping.R
@@ -14,6 +14,8 @@ lib_union <- st_union (lib_buffer)
 
 # need to find a way to filter buffer
 
+?tm_layout
+
 # Toronto 
 tm_shape(filter(CMAs, name == "Toronto (B)")) + 
   tm_borders(col = 'black') +
@@ -21,9 +23,16 @@ tm_shape(filter(CTs, CMA_UID == "35535")) +
   tm_polygons(c("v_CA16_2354: Percentage with employment income", "Population"), border.alpha = 0)  + 
   tm_facets(sync = TRUE, ncol = 2) +
 tm_shape(lib_union)+
-  tm_borders(col = 'black')
+  tm_borders(col = 'black') + 
+  tm_layout(main.title = "Toronto", 
+            main.title.position = "left", 
+            title = c("Percentage of Population on EI", "Population"), 
+            legend.position = c("left", "top"),
+            frame = FALSE) +
+  tm_compass()
 
 # Montreal
+
 tm_shape(filter(CMAs, name == "Montréal (B)")) + 
   tm_borders(col = 'black') +
 tm_shape(filter(CTs, name == "Montréal (B)")) +
