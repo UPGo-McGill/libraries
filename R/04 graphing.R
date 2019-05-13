8733e76191d26c17b00b013ed4daa15c94fb0220
amy <amy.bolt@mail.mcgill.ca>
2019-05-13 13:58:37 -0400

    updated

--- R/04 graphing.R
+++ R/04 graphing.R
@@ -4,6 +4,9 @@ source("R/01 helper_functions.R")
 source("R/02 data_import.R")
 source("R/03 service_area_comparison.R")
 
+## need to multiple percentages by 100##
+## fix legends ##
+
 
 ## Tidying data
 
@@ -39,7 +42,6 @@ Canada_summary <- rbind(
 names(Canada_summary) <-
   c("population", "unemployed_pct", "housing_need", "lone_parent", "immigrants",
     "visible_minorities", "date", "geometry")
-
 
 # Making summary graphs of each year using weighted means
 
@@ -85,7 +87,118 @@ tidy_summary %>%
              alpha = 0.2) +
    geom_point(data = filter(tidy_summary, census_variable == "housing_need"),
              aes(x = date, y = value, colour = library), size = 5) + 
-  geom_line(data = filter(tidy_summary, census_variable == "housing_need"),
+   geom_line(data = filter(tidy_summary, census_variable == "housing_need"),
              aes(x = date, y = value, colour = library, group = library),
-            size = 2)
-  
+            size = 2) + 
+   geom_point(data = Canada_summary,
+              aes(x = date, y = housing_need), 
+              size = 5, alpha = 0.5)+
+   geom_line(data = Canada_summary,
+             aes(x = date, y = housing_need, group = 1),
+             size = 2, alpha = 0.5) +
+   xlab("Year") +
+   ylab("Core Housing Need (%)") +
+   labs(colour = '') +
+   scale_colour_discrete (labels = 
+                         c("Outside Library Service Area", 
+                           "Within Library Service Area"))
+ 
+ # Mapping lone parent
+ ggplot()+
+   geom_line(data = library_service_comparison,
+             aes(x = date, y = lone_parent, colour = library,
+                 group = interaction(library, CMA_name)), 
+             alpha = 0.2) +
+   geom_point(data = library_service_comparison,
+              aes(x = date, y = lone_parent, colour = library), 
+              alpha = 0.2) +
+   geom_point(data = filter(tidy_summary, census_variable == "lone_parent"),
+              aes(x = date, y = value, colour = library), size = 5) + 
+   geom_line(data = filter(tidy_summary, census_variable == "lone_parent"),
+             aes(x = date, y = value, colour = library, group = library),
+             size = 2) + 
+   geom_point(data = Canada_summary,
+              aes(x = date, y = lone_parent), 
+              size = 3, colour = 'purple', alpha = 0.5)+
+   xlab("Year") +
+   ylab("Lone Parent Families (%)") 
+
+ # Mapping immigrants
+ ggplot()+
+   geom_line(data = library_service_comparison,
+             aes(x = date, y = immigrants, colour = library,
+                 group = interaction(library, CMA_name)), 
+             alpha = 0.2) +
+   geom_point(data = library_service_comparison,
+              aes(x = date, y = immigrants, colour = library), 
+              alpha = 0.2) +
+   geom_point(data = filter(tidy_summary, census_variable == "immigrants"),
+              aes(x = date, y = value, colour = library), size = 5) + 
+   geom_line(data = filter(tidy_summary, census_variable == "immigrants"),
+             aes(x = date, y = value, colour = library, group = library),
+             size = 2) + 
+   geom_point(data = Canada_summary,
+              aes(x = date, y = immigrants), 
+              size = 3, colour = 'purple', alpha = 0.5)+
+   xlab("Year") +
+   ylab("Immigrant Population (%)") 
+ 
+ # Mapping visible minorities
+ ggplot()+
+   geom_line(data = library_service_comparison,
+             aes(x = date, y = visible_minorities, colour = library,
+                 group = interaction(library, CMA_name)), 
+             alpha = 0.2) +
+   geom_point(data = library_service_comparison,
+              aes(x = date, y = visible_minorities, colour = library), 
+              alpha = 0.2) +
+   geom_point(data = filter(tidy_summary, census_variable == "visible_minorities"),
+              aes(x = date, y = value, colour = library), size = 5) + 
+   geom_line(data = filter(tidy_summary, census_variable == "visible_minorities"),
+             aes(x = date, y = value, colour = library, group = library),
+             size = 2) + 
+   geom_point(data = Canada_summary,
+              aes(x = date, y = visible_minorities), 
+              size = 3, colour = 'purple', alpha = 0.5)+
+   xlab("Year") +
+   ylab("Visible Minority Population (%)") 
+
+ # Mapping unemployment
+ ggplot()+
+   geom_line(data = library_service_comparison,
+             aes(x = date, y = unemployed_pct, colour = library,
+                 group = interaction(library, CMA_name)), 
+             alpha = 0.2) +
+   geom_point(data = library_service_comparison,
+              aes(x = date, y = unemployed_pct, colour = library), 
+              alpha = 0.2) +
+   geom_point(data = filter(tidy_summary, census_variable == "unemployed_pct"),
+              aes(x = date, y = value, colour = library), size = 5) + 
+   geom_line(data = filter(tidy_summary, census_variable == "unemployed_pct"),
+             aes(x = date, y = value, colour = library, group = library),
+             size = 2) + 
+   geom_point(data = Canada_summary,
+              aes(x = date, y = unemployed_pct), 
+              size = 3, colour = 'purple', alpha = 0.5) +
+   xlab("Year") +
+   ylab("Unemployment Rate (%)")
+ 
+# Mapping median income
+ ggplot()+
+   geom_line(data = library_service_comparison,
+             aes(x = date, y = med_income, colour = library,
+                 group = interaction(library, CMA_name)), 
+             alpha = 0.2) +
+   geom_point(data = library_service_comparison,
+              aes(x = date, y = med_income, colour = library), 
+              alpha = 0.2) +
+   geom_point(data = filter(tidy_summary, census_variable == "med_income"),
+              aes(x = date, y = value, colour = library), size = 5) + 
+   geom_line(data = filter(tidy_summary, census_variable == "med_income"),
+             aes(x = date, y = value, colour = library, group = library),
+             size = 2) + 
+   geom_point(data = Canada_summary,
+              aes(x = date, y = med_income), 
+              size = 3, colour = 'purple', alpha = 0.5)+
+   xlab("Year") +
+   ylab("Median Household Income ($)") 
