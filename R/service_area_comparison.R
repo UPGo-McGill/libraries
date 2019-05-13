12fcf5693d6f4dcf58da51493e2afffa511b3614
David Wachsmuth <david.wachsmuth@mcgill.ca>
2019-05-13 10:58:25 -0400

    Fix for units and unemployed_pct issues, and first stab at simpler weighted summaries

--- R/service_area_comparison.R
+++ R/service_area_comparison.R
@@ -11,7 +11,9 @@ library_service_comparison_2016 <- st_intersect_summarize(
   population = population,
   sum_vars = vars(housing_need, lone_parent, immigrants, visible_minorities),
   mean_vars = vars(unemployed_pct, med_income)
-)
+) %>% 
+  mutate(unemployed_pct = unemployed_pct * 0.01) %>% 
+  drop_units()
 
 library_service_comparison_2006 <- st_intersect_summarize(
   CTs_2006,
@@ -20,7 +22,10 @@ library_service_comparison_2006 <- st_intersect_summarize(
   population = population,
   sum_vars = vars(housing_need, lone_parent, immigrants, visible_minorities),
   mean_vars = vars(unemployed_pct, med_income)
-)
+) %>% 
+  mutate(unemployed_pct = unemployed_pct * 0.01) %>% 
+  drop_units()
+
 
 ## Summaries for the two years
 
@@ -42,41 +47,47 @@ summary_2006_unweighted <-
 
 
 ## Summaries by weighted mean
+
 true_2016 <- filter(library_service_comparison_2016, library == TRUE)
 false_2016 <- filter(library_service_comparison_2016, library == FALSE)
 
-summary_2016_weighted <- data.frame("library" = c(TRUE, FALSE), 
-                                    "housing_need" = c(weighted.mean(true_2016$housing_need, true_2016$population),
-                                                      weighted.mean(false_2016$housing_need, false_2016$population)),
-                                    "lone_parent" = c(weighted.mean(true_2016$lone_parent, true_2016$population),
-                                                       weighted.mean(false_2016$lone_parent, false_2016$population)),
-                                    "immigrants"= c(weighted.mean(true_2016$immigrants, true_2016$population),
-                                                    weighted.mean(false_2016$immigrants, false_2016$population)),
-                                    "visible_minorities"= c(weighted.mean(true_2016$visible_minorities, true_2016$population),
-                                                            weighted.mean(false_2016$visible_minorities, false_2016$population)),
-                                    "unemployed_pct"= c(weighted.mean(true_2016$unemployed_pct, true_2016$population),
-                                                        weighted.mean(false_2016$unemployed_pct, false_2016$population)),
-                                    "med_income" = c(weighted.mean(true_2016$med_income, true_2016$population),
-                                                      weighted.mean(false_2016$med_income, false_2016$population))) %>% 
-  
+summary_2016_weighted <- data.frame(
+  "library" = c(TRUE, FALSE), 
+  "housing_need" = c(weighted.mean(true_2016$housing_need, true_2016$population),
+                     weighted.mean(false_2016$housing_need, false_2016$population)),
+  "lone_parent" = c(weighted.mean(true_2016$lone_parent, true_2016$population),
+                    weighted.mean(false_2016$lone_parent, false_2016$population)),
+  "immigrants"= c(weighted.mean(true_2016$immigrants, true_2016$population),
+                  weighted.mean(false_2016$immigrants, false_2016$population)),
+  "visible_minorities"= c(weighted.mean(true_2016$visible_minorities, true_2016$population),
+                          weighted.mean(false_2016$visible_minorities, false_2016$population)),
+  "unemployed_pct"= c(weighted.mean(true_2016$unemployed_pct, true_2016$population),
+                      weighted.mean(false_2016$unemployed_pct, false_2016$population)),
+  "med_income" = c(weighted.mean(true_2016$med_income, true_2016$population),
+                   weighted.mean(false_2016$med_income, false_2016$population))) 
 
+library_service_comparison_2016 %>% 
+  group_by(library) %>% 
+  summarize_at(c("housing_need", "lone_parent", "immigrants", "visible_minorities",
+              "unemployed_pct", "med_income"), ~{weighted.mean(., population)})
 
 true_2006 <- filter(library_service_comparison_2006, library == TRUE)
 false_2006 <- filter(library_service_comparison_2006, library == FALSE)
 
-summary_2006_weighted <- data.frame("library" = c(TRUE, FALSE), 
-                                    "housing_need" = c(weighted.mean(true_2006$housing_need, true_2006$population),
-                                                       weighted.mean(false_2006$housing_need, false_2006$population)),
-                                    "lone_parent" = c(weighted.mean(true_2006$lone_parent, true_2006$population),
-                                                      weighted.mean(false_2006$lone_parent, false_2006$population)),
-                                    "immigrants"= c(weighted.mean(true_2006$immigrants, true_2006$population),
-                                                    weighted.mean(false_2006$immigrants, false_2006$population)),
-                                    "visible_minorities"= c(weighted.mean(true_2006$visible_minorities, true_2006$population),
-                                                            weighted.mean(false_2006$visible_minorities, false_2006$population)),
-                                    "unemployed_pct"= c(weighted.mean(true_2006$unemployed_pct, true_2006$population),
-                                                        weighted.mean(false_2006$unemployed_pct, false_2006$population)),
-                                    "med_income" = c(weighted.mean(true_2006$med_income, true_2006$population),
-                                                     weighted.mean(false_2006$med_income, false_2006$population)))
+summary_2006_weighted <- data.frame(
+  "library" = c(TRUE, FALSE), 
+  "housing_need" = c(weighted.mean(true_2006$housing_need, true_2006$population),
+                     weighted.mean(false_2006$housing_need, false_2006$population)),
+  "lone_parent" = c(weighted.mean(true_2006$lone_parent, true_2006$population),
+                    weighted.mean(false_2006$lone_parent, false_2006$population)),
+  "immigrants"= c(weighted.mean(true_2006$immigrants, true_2006$population),
+                  weighted.mean(false_2006$immigrants, false_2006$population)),
+  "visible_minorities"= c(weighted.mean(true_2006$visible_minorities, true_2006$population),
+                          weighted.mean(false_2006$visible_minorities, false_2006$population)),
+  "unemployed_pct"= c(weighted.mean(true_2006$unemployed_pct, true_2006$population),
+                      weighted.mean(false_2006$unemployed_pct, false_2006$population)),
+  "med_income" = c(weighted.mean(true_2006$med_income, true_2006$population), 
+                   weighted.mean(false_2006$med_income, false_2006$population)))
 
 rm(true_2016, false_2016, true_2006, false_2006)
 
